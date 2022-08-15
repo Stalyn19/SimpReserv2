ec2c026eb588c6d64164aed876c3d484c1aff0bf
Stalyn Rosario <59269402+Stalyn19@users.noreply.github.com>
2022-08-15 00:32:45 -0400

    g
    t

--- src/main/java/com/simpreserv/repository/RoomRepository.java
+++ src/main/java/com/simpreserv/repository/RoomRepository.java
@@ -2,6 +2,7 @@ package com.simpreserv.repository;
 
 import com.simpreserv.model.Employee;
 import com.simpreserv.model.Room;
+import com.simpreserv.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -15,4 +16,6 @@ public interface RoomRepository extends JpaRepository<Room, Integer> {
   @Query("SELECT e FROM Room e WHERE e.roomNumber = ?1")
   Room findByRoomNumber(String roomNumber);
 
+  @Query("SELECT r FROM Room r WHERE CONCAT(r.roomNumber, r.floor, r.roomPrice, r.roomStatus) LIKE %?1%")
+  public List<Room> findAll(String keyword);
 }
